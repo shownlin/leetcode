56e8f6399c0f75dbd47b9b970ddd051d88882e0e
shownlin <shownlin001cm@gmail.com>
2020-08-11 16:05:24 +0800

    Add Two Numbers

---
+++ 2. Add Two Numbers.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using std::cout;
+
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        ListNode *res = new ListNode();
+        int carry{ 0 };
+        auto p = res;
+        while(l1 || l2 || carry)
+        { 
+            int sum = (l1==nullptr?0:l1->val) + (l2==nullptr?0:l2->val) + carry;
+            p = p->next = new ListNode(sum % 10);
+            if (l1)
+                l1 = l1->next;
+            if(l2)
+                l2 = l2->next;
+            carry = sum / 10;
+        }
+        return res->next;
+    }
+};
+
+int main()
+{
+    ListNode *list1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode *list2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    Solution sol;
+    ListNode *ans = sol.addTwoNumbers(list1, list2);
+    for (auto it = ans; it != nullptr; it = it->next)
+    {
+        cout << it->val << " ";
+    }
+    delete list1;
+    delete list2;
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # leetcode
 Aug. 2020 
+
+# LeetCode Problem List
+
+# Hash Table
+
+| Id | Name | Date |
+|:---:| :------: | :----: |
+| 1 | Two Sum | 8/11 |
+
+# Linked List
+
+| Id | Name | Date |
+|:---:| :------: | :----: |
+| 2 | Add Two Numbers | 8/12 |
+
